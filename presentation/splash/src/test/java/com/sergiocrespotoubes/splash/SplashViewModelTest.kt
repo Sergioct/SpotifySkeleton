3dc1f1bd92721fb7f52c883508aeda98be337f7a
SergioCT <sergio.crespo@abancagen.com>
2024-10-09 18:20:21 +0200

    SplashViewModelTest

--- presentation/splash/src/test/java/com/sergiocrespotoubes/splash/SplashViewModelTest.kt
+++ presentation/splash/src/test/java/com/sergiocrespotoubes/splash/SplashViewModelTest.kt
@@ -1,8 +1,11 @@
 package com.sergiocrespotoubes.splash
 
 import app.cash.turbine.test
+import com.sergiocrespotoubes.domain.model.AuthInfoModel
 import com.sergiocrespotoubes.domain.usecase.user.GetAuthInfoUseCase
 import com.sergiocrespotoubes.testing.MainCoroutineRule
+import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.advanceTimeBy
@@ -21,10 +24,27 @@ class SplashViewModelTest {
 
     @Test
     fun `Given count down timer When is finished Then NavigateToArtistSearch`() = runTest {
+
+        val authInfoModel = AuthInfoModel(accessToken = "123456")
+        coEvery { getAuthInfoUseCase.execute() } returns Result.success(authInfoModel)
+
         val viewModel = SplashViewModel(getAuthInfoUseCase)
         viewModel.event.test {
             advanceTimeBy(2000)
             assertTrue { awaitItem() is SplashViewModel.Event.NavigateToArtistSearch }
+        }
+    }
+
+    @Test
+    fun `Given count down timer When get auth error Then show error state`() = runTest {
+        coEvery { getAuthInfoUseCase.execute() } returns Result.failure(Throwable())
+
+        val viewModel = SplashViewModel(getAuthInfoUseCase)
+        viewModel.event.test {
+            assertTrue { awaitItem() is SplashViewModel.Event.ShowError }
+        }
+        viewModel.state.test {
+            assertTrue { awaitItem() is SplashViewModel.State.Error }
         }
     }
 }
