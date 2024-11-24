964a09dde81a9309ba3a72f3a24ee01e8c655c65
SergioCT <sergio.crespo@abancagen.com>
2024-11-24 18:25:09 +0100

    feature(convention): Trying to implement AndroidApplicationComposeConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -3,12 +3,17 @@ plugins {
 }
 
 dependencies {
+    //compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.gradlePlugin.kotlin)
     compileOnly(libs.gradlePlugin.spotless)
 }
 
 gradlePlugin {
     plugins {
+        /*register("androidApplicationCompose") {
+            id = "com.spotifyskeleton.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }*/
         register("root"){
             id = "com.spotifyskeleton.root"
             implementationClass = "com.sergiocrespotoubes.convention.RootConventionPlugin"
--- build-logic/convention/src/main/java/com/sergiocrespotoubes/convention/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/sergiocrespotoubes/convention/AndroidApplicationConventionPlugin.kt
@@ -1,4 +1,15 @@
 package com.sergiocrespotoubes.convention
 
-class AndroidApplicationConventionPlugin {
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        /*with(target) {
+            pluginManager.apply("com.android.application")
+            val extension = extensions.getByType<ApplicationExtension>()
+            configureAndroidCompose(extension)
+        }*/
+    }
 }
