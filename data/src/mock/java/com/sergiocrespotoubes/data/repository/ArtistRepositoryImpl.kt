ea10e9b0d77e2b9a47b32ac14883c216a7503c56
SergioCT <sergio.crespo@abancagen.com>
2025-03-05 00:59:02 +0100

    refactor(ArtistDetail): spotless

--- data/src/mock/java/com/sergiocrespotoubes/data/repository/ArtistRepositoryImpl.kt
+++ data/src/mock/java/com/sergiocrespotoubes/data/repository/ArtistRepositoryImpl.kt
@@ -30,17 +30,17 @@ class ArtistRepositoryImpl
                 )
             }
 
-    override suspend fun getArtistByArtistId(artistId: String): Flow<Result<ArtistModel>> =
-        flow {
-            emit(
-                Result.success(
-                    ArtistModel(
-                        id = "1",
-                        name = "Artist 1",
-                        popularity = 0,
-                        urlPicture = "",
+        override suspend fun getArtistByArtistId(artistId: String): Flow<Result<ArtistModel>> =
+            flow {
+                emit(
+                    Result.success(
+                        ArtistModel(
+                            id = "1",
+                            name = "Artist 1",
+                            popularity = 0,
+                            urlPicture = "",
+                        ),
                     ),
-                ),
-            )
-        }
-}
+                )
+            }
+    }
